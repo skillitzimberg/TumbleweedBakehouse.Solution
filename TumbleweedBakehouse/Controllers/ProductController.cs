cc60e13d690bdc6cbea44cadbd335046736e6627
Alex Garcia <g.nightster11@gmail.com>
2018-12-14 10:21:18 -0800

    commit for solidarity

--- TumbleweedBakehouse/Controllers/ProductController.cs
+++ TumbleweedBakehouse/Controllers/ProductController.cs
@@ -9,12 +9,57 @@ namespace TumbleweedBakehouse.Controllers
         [HttpGet("/product")]
         public ActionResult Index()
         {
-          Product newProductOne = new Product("sourdough","raye","light and fluffy","URL",true,3,1);
-          Product newProductTwo = new Product("Honeybread","Wheat","hearty and sweet","URL",true,5,2);
-          List<Product> productList = new List<Product> { newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo };
 
-          return View(productList);
+          List<Product> newList = new List<Product>{};
+          newList = Product.GetAll();
+
+          return View(newList);
         }
+
+        [HttpGet("/product/new")]
+        public ActionResult New()
+        {
+          return View();
+        }
+
+        [HttpPost("/product")]
+        public ActionResult Create(string name, string type, string description, string  url, bool availability, float price)
+        {
+          Product newProduct = new Product (name, type, description, url, availability, price);
+
+          return View("show",newProduct);
+
+        }
+
+        // [HttpPost("/product/show")]
+        // public ActionResult Show()
+        // {
+        //
+        // }
+
+
+
+
+        //
+        // Dictionary<string, object> model = new Dictionary<string, object>{};
+        // Product selectProduct = Product.Find(id);
+        // List<Product> allProducts = Product.GetAll();
+        // model.Add("selectProduct",selectProduct);
+        // model.Add("allProducts", allProducts);
+        //
+
+
+
+        // Product newProductOne = new Product("sourdough","raye","light and fluffy","URL",true,3,1);
+        // Product newProductTwo = new Product("Honeybread","Wheat","hearty and sweet","URL",true,5,2);
+        // List<Product> productList = new List<Product> { newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo, newProductOne, newProductTwo };
+
+
+
+
+
+
+
 
     }
 }
