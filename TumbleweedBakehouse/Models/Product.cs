f875a886a36734be50ee706b32a7259622ac078f
Student <example@example.com>
2018-12-19 13:50:22 -0800

    add working availability update feature.

--- TumbleweedBakehouse/Models/Product.cs
+++ TumbleweedBakehouse/Models/Product.cs
@@ -199,6 +199,7 @@ namespace TumbleweedBakehouse.Models
 
         public void Edit(string name, string producttype, string description, string url, bool availability, float price)
         {
+          // availability = true;
           MySqlConnection conn = DB.Connection();
           conn.Open();
           var cmd = conn.CreateCommand() as MySqlCommand;
